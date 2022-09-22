3b31659285ba0a3c47aa253e170bf5e953e835fa
Abduino <abdrehmanredi0@gmail.com>
2022-09-22 16:50:44 -0300

    typescript hello world

---
+++ hello_world/index.js
@@ -0,0 +1,17 @@
+console.log("hello world");
+if (5 == 5) {
+    console.log("good");
+}
+
+for( var i =0; i<=5; i++){
+    console.log(i)
+}
+
+const msg ="new message"
+
+console.log(msg)
+
+function fun(){
+    console.log("function")
+}
+fun()
---
+++ hello_world/index.ts
@@ -0,0 +1,18 @@
+
+console.log("hello world");
+if (5 == 5) {
+    console.log("good");
+}
+
+
+
+const msg ="new message"
+
+console.log(msg)
+
+function fun(){
+    console.log("function")
+}
+for( var i =0; i<=5; i++){
+    fun()
+}
