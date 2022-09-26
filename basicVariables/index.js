fd2232dbd1ad425572775a37406a12ad910bd86b
Abduino <abdrehmanredi0@gmail.com>
2022-09-26 14:42:36 -0300

    Typescript basic data types

---
+++ basicVariables/index.js
@@ -0,0 +1,12 @@
+// typescript basic data types 
+var age = 25; // intiger 
+var lname = "abdurehman"; // string data type
+var fname = "redi";
+var weight = 62.5;
+var activated = true; // boolean data type
+// display values 
+console.log("First name is= " + fname);
+console.log("Father name is = " + lname);
+console.log("Age = " + age);
+console.log("Weight " + weight);
+console.log("Active user = " + activated);
---
+++ basicVariables/index.ts
@@ -0,0 +1,19 @@
+
+// typescript basic data types 
+
+let age:number = 25; // intiger 
+let lname: string="abdurehman"; // string data type
+let fname: string = "redi"; 
+var weight: number = 62.5;
+let activated : boolean = true;  // boolean data type
+
+
+// display values 
+
+console.log("First name is= " + fname);
+console.log("Father name is = " + lname);
+console.log("Age = " + age);
+console.log("Weight " + weight);
+console.log("Active user = " + activated);
+
+
