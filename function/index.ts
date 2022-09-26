0d57ffa3cf9869589722228ec955d4f359a075fb
Abduino <abdrehmanredi0@gmail.com>
2022-09-26 15:20:35 -0300

    function with zero parameter

---
+++ function/index.ts
@@ -0,0 +1,25 @@
+// function with zero parameter 
+function displayHello(){
+    console.log("hello world")
+}
+
+
+// function with parameter 
+
+function displayUser( fname:string, lname:string, age:number, weight:number ){
+    console.log("First name is= " + fname)
+    console.log("Father name is = " + lname)
+    console.log("Age = " + age)
+    console.log("Weight " + weight)
+}
+
+//calling a function
+
+displayHello()
+displayUser("abdurehman", "redi", 50, 62.5)
+
+
+
+
+
+
