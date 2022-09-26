08a46f492515222dae4ac9d202b698f8f30b810c
Abduino <abdrehmanredi0@gmail.com>
2022-09-26 14:44:30 -0300

    typescript looping statements

---
+++ loopStatements/index.js
@@ -0,0 +1,18 @@
+// typescript looping statements 
+var i;
+//for loop statement
+for (i = 0; i < 10; i++) {
+    console.log("Iteration" + i);
+}
+//while statement 
+i = 0;
+while (i <= 10) {
+    console.log("iteration" + i);
+    i++;
+}
+//do while statements
+i = 0;
+do {
+    console.log("Iteration" + i);
+    i++;
+} while (i <= 10);
---
+++ loopStatements/index.ts
@@ -0,0 +1,24 @@
+// typescript looping statements 
+
+var i:number;
+
+//for loop statement
+for(i=0; i < 10 ; i++){
+    console.log("Iteration" + i )
+}
+
+//while statement 
+
+i = 0
+while(i <= 10){
+    console.log("iteration" + i)
+    i++
+}
+
+
+//do while statements
+i = 0
+do{
+    console.log("Iteration" + i)
+    i++
+}while(i <= 10)
